fb581bf6de42c138a835d9e6922a978a3bf0d0a6
Steven Robertson <steve@tiepy.com>
2020-06-15 17:07:36 +0100

    Adding copyright notice and GPL

--- src/main/java/com/tiepy/nimatron/NimParserDefinition.java
+++ src/main/java/com/tiepy/nimatron/NimParserDefinition.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright 2020 TiePy Ltd. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Nimatron, a Nim plug-in for IDEA platform IDEs.
+ *
+ * Nimatron is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.  TiePy Ltd designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by TiePy Ltd in the LICENSE file that accompanied this code.
+ *
+ * Nimatron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nimatron.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Please contact TiePy Ltd, 19 Lyall Way, Laurencekirk, AB30 1FU, UK
+ * or visit www.tiepy.com if you need additional information or have any
+ * questions.
+ */
+
 package com.tiepy.nimatron;
 
 import com.intellij.lang.*;
