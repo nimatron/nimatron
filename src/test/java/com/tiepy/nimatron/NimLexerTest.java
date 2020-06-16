7d7aa5f35ac98fd1bb8dcf6d7022962eba6ff833
Steven Robertson <steven.robertson@tiepy.com>
2020-06-16 19:46:42 +0100

    Adding more to the lexer test

--- src/test/java/com/tiepy/nimatron/NimLexerTest.java
+++ src/test/java/com/tiepy/nimatron/NimLexerTest.java
@@ -27,23 +27,87 @@
 package com.tiepy.nimatron;
 
 import com.intellij.psi.tree.IElementType;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NimLexerTest {
 
-    @Test
-    public void test() {
+    private String stringify(String input) {
+        return input.replace("\n", "\\n");
+    }
+
+    /**
+     * Parses the string into tokens using the lexer to be tested.
+     *
+     * @param input String to be parsed.
+     * @return Token count.
+     */
+    private int parse(String input) {
+        int count = 0;
+
         NimLexerAdapter lexer = new NimLexerAdapter();
-        String str = "echo \"Hello, World!\"";
+
         int start = 0;
-        int end = str.length();
-        lexer.start(str, start, end, NimLexer.YYINITIAL);
+        int end = input.length();
+        lexer.start(input, start, end, NimLexer.YYINITIAL);
 
         IElementType token = lexer.getTokenType();
         while (token != null) {
-            System.out.println(String.format("%s \"%s\"", token.toString(), lexer.getTokenText()));
+            count++;
+            System.out.println(String.format("%s \"%s\"", token.toString(), stringify(lexer.getTokenText())));
             lexer.advance();
             token = lexer.getTokenType();
         }
+
+        return count;
+    }
+
+    @Test
+    public void test() {
+        int tokenCount = parse("echo \"Hello, World!\" # This is a line comment\n" +
+                "\n" +
+                "#[\n" +
+                "This is a block comment.\n" +
+                "#[\n" +
+                "This is a nested block comment.\n" +
+                "]#\n" +
+                "]#\n" +
+                "\n" +
+                "proc p(arg: int): int =\n" +
+                "  s.add $arg\n" +
+                "  result = arg\n" +
+                "\n" +
+                "discard p(p(1) + p(2))\n" +
+                "\n" +
+                "var b = true\n" +
+                "doAssert b == true\n" +
+                "\n" +
+                "if true else false\n" +
+                "\n" +
+                "\"\"\"This is\n" +
+                "a multi-line string that can contain \"Other strings\".\"\"\"\n" +
+                "\n" +
+                "var path = r\"C:\\\"; echo path\n" +
+                "\n" +
+                "var s1 = abc\"string literal\\\"\n" +
+                "var s2 = ident\"\"\"string literal\"\"\"\n" +
+                "var c = 'c'\n" +
+                "\n" +
+                "var n1 = 123\n" +
+                "var n2 = 1.2\n" +
+                "var n3 = 1e23\n" +
+                "var n4 = 1.2e-23\n" +
+                "var test = true and false\n" +
+                "\n" +
+                "return json({'hello': 'world'})\n" +
+                "\n" +
+                "var\n" +
+                "  x, y: int\n" +
+                "  # a comment can occur here too\n" +
+                "  a, b, c: string\n" +
+                "\n" +
+                "var checkpoints* {.threadvar.}: seq[string]\n");
+
+        Assert.assertEquals(215, tokenCount);
     }
 }
