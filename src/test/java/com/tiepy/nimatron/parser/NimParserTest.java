81bbb3f52fc53607b60697e4f2282583fb7e4dbc
Steven Robertson <steven.robertson@tiepy.com>
2020-06-16 01:41:00 +0100

    Adding an example for testing parser

---
+++ src/test/java/com/tiepy/nimatron/parser/NimParserTest.java
@@ -0,0 +1,63 @@
+/*
+ * This file is based on example provided by the IntelliJ Platform SDK DevGuide.
+ * Copyright 2000-2020 JetBrains s.r.o. and other contributors.
+ * Use of original example source code is governed by the Apache 2.0 license.
+ *
+ * Copyright 2020 TiePy Ltd. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Nimatron, a Nim plug-in for IDEA platform IDEs.
+ *
+ * Nimatron is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.  TiePy Ltd designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by TiePy Ltd in the LICENSE file that accompanied this code.
+ *
+ * Nimatron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nimatron.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Please contact TiePy Ltd, 19 Lyall Way, Laurencekirk, AB30 1FU, UK
+ * or visit www.tiepy.com if you need additional information or have any
+ * questions.
+ */
+
+package com.tiepy.nimatron.parser;
+
+import com.intellij.testFramework.ParsingTestCase;
+import com.tiepy.nimatron.NimParserDefinition;
+
+public class NimParserTest extends ParsingTestCase {
+    public NimParserTest() {
+        super("", "nim", new NimParserDefinition());
+    }
+
+    public void testParsingTestData() {
+        doTest(true);
+    }
+
+    /**
+     *
+     * @return path to test data file directory relative to root of this module.
+     */
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData";
+    }
+
+    @Override
+    protected boolean skipSpaces() {
+        return false;
+    }
+
+    @Override
+    protected boolean includeRanges() {
+        return true;
+    }
+}
