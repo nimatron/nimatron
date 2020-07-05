59404479a7b6a2400779b59adc816cc9da9cc918
Steven Robertson <steven.robertson@tiepy.com>
2020-07-05 21:20:48 +0100

    Defining classes for structure view

---
+++ src/main/java/com/tiepy/nimatron/NimStructureViewElement.java
@@ -0,0 +1,101 @@
+/*
+ * This file is based on example provided by the IntelliJ Platform SDK DevGuide.
+ * Copyright 2000-2020 JetBrains s.r.o. and other contributors.
+ * Use of original example source code is governed by the Apache 2.0 license.
+ *
+ * Copyright 2020 TiePy Ltd. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Nimatron, a Nim plug-in for IDEA platform IDEs.
+ *
+ * Nimatron is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.  TiePy Ltd designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by TiePy Ltd in the LICENSE file that accompanied this code.
+ *
+ * Nimatron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nimatron.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Please contact TiePy Ltd, 19 Lyall Way, Laurencekirk, AB30 1FU, UK
+ * or visit www.tiepy.com if you need additional information or have any
+ * questions.
+ */
+
+package com.tiepy.nimatron;
+
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.SortableTreeElement;
+import com.intellij.ide.util.treeView.smartTree.TreeElement;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.psi.NavigatablePsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NimStructureViewElement implements StructureViewTreeElement, SortableTreeElement {
+    private final NavigatablePsiElement myElement;
+
+    public NimStructureViewElement(NavigatablePsiElement element) {
+        this.myElement = element;
+    }
+
+    @Override
+    public Object getValue() {
+        return myElement;
+    }
+
+    @Override
+    public void navigate(boolean requestFocus) {
+        myElement.navigate(requestFocus);
+    }
+
+    @Override
+    public boolean canNavigate() {
+        return myElement.canNavigate();
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+        return myElement.canNavigateToSource();
+    }
+
+    @NotNull
+    @Override
+    public String getAlphaSortKey() {
+        String name = myElement.getName();
+        return name != null ? name : "";
+    }
+
+    @NotNull
+    @Override
+    public ItemPresentation getPresentation() {
+        ItemPresentation presentation = myElement.getPresentation();
+        return presentation != null ? presentation : new PresentationData();
+    }
+
+    @NotNull
+    @Override
+    public TreeElement[] getChildren() {
+/*
+        if (myElement instanceof NimFile) {
+            List<NimProperty> properties = PsiTreeUtil.getChildrenOfTypeAsList(myElement, NimProperty.class);
+            List<TreeElement> treeElements = new ArrayList<>(properties.size());
+            for (NimProperty property : properties) {
+                treeElements.add(new NimStructureViewElement((NimPropertyImpl) property));
+            }
+            return treeElements.toArray(new TreeElement[0]);
+        }
+*/
+        return EMPTY_ARRAY;
+    }
+}
---
+++ src/main/java/com/tiepy/nimatron/NimStructureViewModel.java
@@ -0,0 +1,59 @@
+/*
+ * This file is based on example provided by the IntelliJ Platform SDK DevGuide.
+ * Copyright 2000-2020 JetBrains s.r.o. and other contributors.
+ * Use of original example source code is governed by the Apache 2.0 license.
+ *
+ * Copyright 2020 TiePy Ltd. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Nimatron, a Nim plug-in for IDEA platform IDEs.
+ *
+ * Nimatron is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.  TiePy Ltd designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by TiePy Ltd in the LICENSE file that accompanied this code.
+ *
+ * Nimatron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nimatron.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Please contact TiePy Ltd, 19 Lyall Way, Laurencekirk, AB30 1FU, UK
+ * or visit www.tiepy.com if you need additional information or have any
+ * questions.
+ */
+
+package com.tiepy.nimatron;
+
+import com.intellij.ide.structureView.*;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+public class NimStructureViewModel extends StructureViewModelBase implements
+        StructureViewModel.ElementInfoProvider {
+    public NimStructureViewModel(PsiFile psiFile) {
+        super(psiFile, new NimStructureViewElement(psiFile));
+    }
+
+    @NotNull
+    public Sorter[] getSorters() {
+        return new Sorter[]{Sorter.ALPHA_SORTER};
+    }
+
+
+    @Override
+    public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+        return false;
+    }
+
+    @Override
+    public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+        return element instanceof NimFile;
+    }
+}
