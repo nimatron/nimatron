0eb7d4082791512aff5824fc52d62eb4a243b8e3
Steven Robertson <steven.robertson@tiepy.com>
2020-07-12 15:36:26 +0100

    Replacing PNG file icon with SVG icon

--- src/main/java/com/tiepy/nimatron/NimIcons.java
+++ src/main/java/com/tiepy/nimatron/NimIcons.java
@@ -35,5 +35,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class NimIcons {
-    public static final Icon FILE = IconLoader.getIcon("/icons/nim16x16.png");
+    public static final Icon FILE = IconLoader.getIcon("/icons/nim.svg");
 }
