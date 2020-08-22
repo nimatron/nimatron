b95496e647f67530f1951ad2841592f72a402d04
Steven Robertson <steven.robertson@tiepy.com>
2020-08-22 16:19:14 +0100

    Bringing the root rules closer to the Nim grammar spec style

--- src/main/gen/com/tiepy/nimatron/psi/NimStmt.java
+++ src/main/gen/com/tiepy/nimatron/psi/NimStmt.java
@@ -7,70 +7,7 @@ import com.intellij.psi.PsiElement;
 
 public interface NimStmt extends NimElement {
 
-  @Nullable
-  NimAsmStmt getAsmStmt();
-
-  @Nullable
-  NimBindStmt getBindStmt();
-
-  @Nullable
-  NimBlockStmt getBlockStmt();
-
-  @Nullable
-  NimConstStmt getConstStmt();
-
-  @Nullable
-  NimConverterStmt getConverterStmt();
-
-  @Nullable
-  NimDeferStmt getDeferStmt();
-
-  @Nullable
-  NimForStmt getForStmt();
-
-  @Nullable
-  NimFuncStmt getFuncStmt();
-
-  @Nullable
-  NimIfStmt getIfStmt();
-
-  @Nullable
-  NimIteratorStmt getIteratorStmt();
-
-  @Nullable
-  NimMacroStmt getMacroStmt();
-
-  @Nullable
-  NimMethodStmt getMethodStmt();
-
-  @Nullable
-  NimMixinStmt getMixinStmt();
-
-  @Nullable
-  NimProcStmt getProcStmt();
-
-  @Nullable
-  NimSimpleStmt getSimpleStmt();
-
-  @Nullable
-  NimStaticStmt getStaticStmt();
-
-  @Nullable
-  NimTemplateStmt getTemplateStmt();
-
-  @Nullable
-  NimTryStmt getTryStmt();
-
-  @Nullable
-  NimTypeStmt getTypeStmt();
-
-  @Nullable
-  NimVarStmt getVarStmt();
-
-  @Nullable
-  NimWhenStmt getWhenStmt();
-
-  @Nullable
-  NimWhileStmt getWhileStmt();
+  @NotNull
+  NimComplexOrSimpleStmt getComplexOrSimpleStmt();
 
 }
